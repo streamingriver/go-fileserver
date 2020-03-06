7061b8fa5b075c6e2c3e543344f9e872df21eaa3
Mindaugas Žvirblis <mz@9g.lt>
2020-03-06 10:44:12 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/streamingriver/go-fileserver
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	flagBindTo = flag.String("bind-to", "localhost:9005", "bind to ")
+	flagPath   = flag.String("path", "/dev/shm", "path to serve")
+)
+
+func main() {
+	flag.Parse()
+	http.Handle("/", http.FileServer(http.Dir(*flagPath)))
+	log.Printf("Starting server on %s", *flagBindTo)
+	log.Fatal(http.ListenAndServe(*flagBindTo), nil)
+}
